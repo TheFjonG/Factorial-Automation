8e19b0dc07a7080723e12df0185744239ed91487
Amadornes <amadornes@gmail.com>
2014-10-18 13:14:00 +0200

    Added pipe part base

---
+++ src/main/java/com/thefjong/factorialautomation/part/pipe/PartPipe.java
@@ -0,0 +1,37 @@
+package com.thefjong.factorialautomation.part.pipe;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.qmunity.lib.vec.Vec3d;
+import com.qmunity.lib.vec.Vec3dCube;
+import com.thefjong.factorialautomation.part.FAPart;
+import com.thefjong.factorialautomation.reference.Reference;
+import com.thefjong.factorialautomation.reference.ReferenceParts;
+
+public class PartPipe extends FAPart {
+
+    @Override
+    public String getType() {
+
+        return ReferenceParts.PIPE;
+    }
+
+    @Override
+    public String getUnlocalizedName() {
+
+        return Reference.MODID + ".part." + getType();
+    }
+
+    @Override
+    public void renderDynamic(Vec3d translation, double delta, int pass) {
+
+    }
+
+    @Override
+    public List<Vec3dCube> getSelectionBoxes() {
+
+        return Arrays.asList(new Vec3dCube(0, 0, 0, 1, 1, 1));
+    }
+
+}
